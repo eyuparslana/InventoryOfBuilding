a13bf2b096bc0a3f00d6366053c1ca8a4f2f8f92
eyuparslana <eyuparslana@gmail.com>
2020-01-29 19:12:42 +0300

    post, put, delete method added for buildings

--- Inventory/views.py
+++ Inventory/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from Inventory.models import *
+from Inventory.models import Building
 from rest_framework.views import APIView
-from .serializer import *
+from .serializer import BuildingSerializer
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from rest_framework import status
@@ -21,7 +21,63 @@ class BuildingsView(APIView):
                             status=status.HTTP_401_UNAUTHORIZED)
 
     def post(self, request):
-        pass
+        if request.user.is_authenticated:
+            if request.data:
+                name = request.data.get('building_name')
+                flat_count = request.data.get('flat_count')
+                if not name or not flat_count:
+                    return Response({'Error': 'Bad Request', 'message': 'invalid name or count'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+                building = Building.objects.filter(building_name=name)
+                if building:
+                    return Response({'Error': 'Conflict', 'message': 'Building already exists'},
+                                    status=status.HTTP_409_CONFLICT)
+                new_building = Building(building_name=name, flat_count=flat_count)
+                new_building.save()
+                return Response({'Status': 'Created', 'message': 'Building Created'}, status.HTTP_201_CREATED)
+        else:
+            return Response({'Error': 'Unauthorized person', 'message': 'Login please'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+
+    def put(self, request):
+        if request.user.is_authenticated:
+            if request.data:
+                name = request.data.get('building_name')
+                flat_count = request.data.get('flat_count')
+                if not name or not flat_count:
+                    return Response({'Error': 'Bad Request', 'message': 'invalid name or count'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+                try:
+                    building = Building.objects.get(building_name=name)
+                    building.flat_count = flat_count
+                    building.save()
+                except Building.DoesNotExist:
+                    return Response({'Error': 'Not Found', 'message': 'Building not found'},
+                                    status=status.HTTP_404_NOT_FOUND)
+
+                return Response({'Status': 'Created', 'message': 'Building Updated'}, status.HTTP_200_OK)
+        else:
+            return Response({'Error': 'Unauthorized person', 'message': 'Login please'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+
+    def delete(self, request):
+        if request.user.is_authenticated:
+            if request.data:
+                name = request.data.get('building_name')
+                if not name:
+                    return Response({'Error': 'Bad Request', 'message': 'invalid name or count'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+                try:
+                    building = Building.objects.get(building_name=name)
+                    building.delete()
+                except Building.DoesNotExist:
+                    return Response({'Error': 'Not Found', 'message': 'Building not found'},
+                                    status=status.HTTP_404_NOT_FOUND)
+
+                return Response({'Status': 'Created', 'message': 'Building deleted'}, status.HTTP_200_OK)
+        else:
+            return Response({'Error': 'Unauthorized person', 'message': 'Login please'},
+                            status=status.HTTP_401_UNAUTHORIZED)
 
 
 class RegisterView(APIView):
